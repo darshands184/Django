c57bb00eab04b8510fb9b6e29e1ae2f801a8b258
Darshan Yenbhar <66029648+darshands184@users.noreply.github.com>
2022-09-13 17:21:57 +0530

    Update views.py

--- text_utils/text_utils/views.py
+++ text_utils/text_utils/views.py
@@ -98,28 +98,6 @@ def ex1(request):
     return HttpResponse(s)
 
 
-# Practice Code 3
-# def ex1(request):
-#     s = '''
-#      <head><link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet"></head>
-#      <style> .h1,.h2,.h3,.h4,.h5,.h6,h1,h2,h3,h4,h5,h6 {
-#         margin-bottom: .5rem;
-#         font-family: 'abel', sans-serif;
-#         font-weight: 700;
-#         line-height: 1.2
-#         } body {
-#         font-size: 1rem;
-#         background-color: Cyan }
-#     </style>
-#      <h1>My Website</h1> <a href="https://rohandas28.github.io/"> Click To Visit My Tiny Little Website!</a>
-#      <h1>Harry Bhai Ka Website </h1> <a href="https://www.codewithharry.com/"> Click To Visit The Best Website!</a>
-#      <h1>My Favourite Movie </h1> <a href= "https://www.google.com/search?q=interstellar&oq=interstellar&aqs=chrome..69i57j69i65.6672j0j1&sourceid=chrome&ie=UTF-8"> Click to See!</a>
-#      <h1>My Favourite Youtube creator </h1> <a href="https://www.youtube.com/channel/UCeVMnSShP_Iviwkknt83cww"> Click To Visit And Subscribe To Him!</a>
-#      <h1>Psst! Follow Me On Github! LoL!</h1> <a href="https://github.com/RohanDas28"> Click To Visit And Follow!</a>
-#     '''
-#     return HttpResponse(s)
-
-
 # Practice Code 2
 # def capitalize_first(request):
 #     return HttpResponse("<h1>Capitalize First</h1>")
